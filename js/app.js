c095a15e240c9da7742a5ccd046ab9bebe53e504
Tracy Williams <selpilot@gmail.com>
2018-04-03 15:34:49 -0700

    Moved all js from html to app.js.

--- js/app.js
+++ js/app.js
@@ -19,6 +19,9 @@ var questions = [ 'Am I the middle sibling in my family?',
 var answers = ['N', 'Y', 'Y', 'N', 'Y']; // array holding corresponding answers
 var score = 0; //keep score
 
+//
+// Ask all the questions and gather answers. Keep score.
+//
 var i = 0; // all of this code belongs in a loop but we don't know how to do that yet.  ;-)
 guess[i] = prompt(questions[i]);
 if ((guess[i].toUpperCase().charAt(0)) === answers[i]) {
@@ -26,30 +29,72 @@ if ((guess[i].toUpperCase().charAt(0)) === answers[i]) {
 }
 console.log(questions[i], 'User input:', guess[i], 'Right anwser:', answers[i],'\n');
 
-i++;
+i++; // 2...
 guess[i] = prompt(questions[i]);
 if ((guess[i].toUpperCase().charAt(0)) === answers[i]) {
   score++;
 }
 console.log(questions[i], 'User input:', guess[i], 'Right anwser:', answers[i],'\n');
 
-i++;
+i++; // 3...
 guess[i] = prompt(questions[i]);
 if ((guess[i].toUpperCase().charAt(0)) === answers[i]) {
   score++;
 }
 console.log(questions[i], 'User input:', guess[i], 'Right anwser:', answers[i],'\n');
 
-i++;
+i++; // 4...
 guess[i] = prompt(questions[i]);
 if ((guess[i].toUpperCase().charAt(0)) === answers[i]) {
   score++;
 }
 console.log(questions[i], 'User input:', guess[i], 'Right anwser:', answers[i],'\n');
 
-i++;
+i++; // 5...
 guess[i] = prompt(questions[i]);
 if ((guess[i].toUpperCase().charAt(0)) === answers[i]) {
   score++;
 }
 console.log(questions[i], 'User input:', guess[i], 'Right anwser:', answers[i],'\n');
+
+//
+// Now use the answers to fill in the html page...
+//
+
+var elIds = ['q1', 'q2', 'q3', 'q4', 'q5'];
+
+i = 0;
+var el = document.getElementById(elIds[i]);
+el.textContent = questions[i] + ' You answered ' + guess[i] + ', the correct answer was ' + answers[i];
+
+i++;
+el = document.getElementById(elIds[i]);
+el.textContent = questions[i] + ' You answered ' + guess[i] + ', the correct answer was ' + answers[i];
+
+i++;
+el = document.getElementById(elIds[i]);
+el.textContent = questions[i] + ' You answered ' + guess[i] + ', the correct answer was ' + answers[i];
+
+i++;
+el = document.getElementById(elIds[i]);
+el.textContent = questions[i] + ' You answered ' + guess[i] + ', the correct answer was ' + answers[i];
+
+i++;
+el = document.getElementById(elIds[i]);
+el.textContent = questions[i] + ' You answered ' + guess[i] + ', the correct answer was ' + answers[i];
+
+//
+// Fill in score and fill in final bit of trash talk.
+//
+el = document.getElementById('score');
+el.textContent = score;
+
+el = document.getElementById('comment-on-score');
+if (score === 5) {
+  el.textContent = 'A perfect score! You must know me pretty well. Nice job.';
+} else if (score >= 3 && score <= 4) {
+  el.textContent = 'Not bad. Better than mere random chance. You pass!';
+} else { // (score <= 2)
+  el.textContent = 'Man, that score blows. You need to have a conversation with me sometime soon and get some answers!';
+}
+
