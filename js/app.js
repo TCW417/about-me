cbb3ccd9ef518bed78a0a51adbc74a7d1f2078c0
Tracy Williams <selpilot@gmail.com>
2018-04-04 14:39:48 -0700

    It's been too long! js includes code for Day 3 excercise. includes two linter errors though. Still chasing them down.

--- js/app.js
+++ js/app.js
@@ -28,13 +28,15 @@ var answers = [
   ['no', 'yes', 'yes', 'no', 'yes']
 ];
 var score = 0; //keep score
-
 var userName; // user's name
+var numberOfGrandKids = 7; // the number to guess
+var guessCountDown = 4; // number of tries to guess it
+var userGuess; // user's guess at the number
 
 //
 // getUsersName
 //
-userName = prompt('Welcome to my About Tracy game!\nWhat\\s your name?');
+userName = prompt('Welcome to my About Tracy game!\n\nWhat\'s your name?');
 if (!confirm('Thanks ' + userName + '! Ready to play my game?')) {
   alert('OK ' + userName + '. No hard feelings! (We\'ll play anyway...');
   // this is where I'd like to abort the script...  
@@ -53,7 +55,69 @@ for (var i = 0; i < questions.length; i++) {
   }
   console.log(questions[i], 'User input:', guess[i], 'Right answer:', answers[1][i],'\n');
 }
+alert('Nicely done ' + userName + '. Ready to play a guessing game?');
 
+
+//Play the number guessing game
+
+console.log('grandKidCountdown',guessCountDown);
+for (guessCountDown = guessCountDown--; grandKidCountDown > 0; grandKidCountDown--) {
+do {
+  userGuess = parseInt(prompt('How many grandkids to I have?'));
+  guessCountDown--;
+  console.log('Grandkid guess:', userGuess, 'Guesses to go', guessCountDown);
+  console.log('typeof input',typeof userGuess);
+  if (!isNaN(userGuess)) { //then they entered a number...
+    if (userGuess === numberOfGrandKids) {
+      alert('That\'s right ' + userName + '! Nicely done.');
+      score++;
+      break;
+    } else if (guessCountDown > 0) {
+      alert('Close ' + userName + ', but your a bit ' + (userGuess > numberOfGrandKids ? 'high.' : 'low. ' + guessCountDown + (guessCountDown === 1 ? ' guess' : ' guesses') + ' to go!'));
+    } else {
+      alert('Close ' + userName + ', but your a bit ' + (userGuess > numberOfGrandKids ? 'high.' : 'low. ') + 'And you\'re out of guesses!');
+    }
+  } else {
+    alert('Please enter a number as your answer. ' + guessCountDown + (guessCountDown === 1 ? ' guess' : ' guesses') + ' to go!');
+  }
+} while (guessCountDown > 0);
+console.log('done guessing');
+
+//
+// guessMyCities...
+//
+// user has 6 tries to guess a city I've lived in...
+//
+var myCities = ['Tacoma', 'Gig Harbor', 'Bellevue', 'Kirkland', 'Randle'];
+var numTries = 6;
+var cityGuess;
+var correctGuess = false;
+
+alert('Okay ' + userName + '. Ready to play another guessing game?\n\nIn this one you have six tries to guess a city I\'ve lived in.  Good luck!');
+
+do {
+  cityGuess = prompt('Name a city I\'ve lived in:');
+  numTries--;
+  console.log('City guess', cityGuess);
+  for (var c = 0; c < myCities.length; c++) {
+    if (cityGuess.toLowerCase() === myCities[c].toLowerCase()) {
+      alert('Gread guess ' + userName + '! ' + myCities[c] + ' is a place I\'ve lived!');
+      correctGuess = true;
+      score++;
+      break;
+    }
+  }
+  if (correctGuess === false) {
+    var alertString = 'No, I haven\'t lived in ' + cityGuess + '. ';
+    if (numTries >= 1) {
+      alertString += 'Try again. You have ' + numTries + (numTries > 1 ? ' guesses' : ' guess') + ' remaining.';
+    }
+    alert(alertString);
+  } else {
+    break;
+  }
+} while (numTries > 0);
+console.log('Finished cityGuess game, correctGuess flag:',correctGuess);
 //
 // updateTheHtml
 //
@@ -61,13 +125,25 @@ for (var i = 0; i < questions.length; i++) {
 // into the html.  Based on their score, some friendly trash talk is appended to the
 // results list.
 //
-var elIds = ['q1', 'q2', 'q3', 'q4', 'q5'];
+var elIds = ['q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7'];
 var el = new Document;
 
 for (var j = 0; j < questions.length; j++) {
   el = document.getElementById(elIds[j]);
   el.textContent = questions[j] + ' You answered ' + guess[j] + ', the correct answer was ' + answers[1][j] + '.';
 }
+
+el = document.getElementById(elIds[5]); //element ID for question 6, number guess
+el.textContent = 'How many grand kids do I have? You guessed ' + (userGuess === numberOfGrandKids ? 'correctly! ' : 'incorrectly.') + ' The answer is ' + numberOfGrandKids + '.';
+
+el = document.getElementById(elIds[6]); //element ID for question 7, City guess
+var cityHtmlCode = 'What cities have I lived in? You guessed ' + cityGuess + '. Here\'s a list of places I\'ve lived in Washinton:<ul>';
+for (var c = 0; c < myCities.length; c++) {
+  cityHtmlCode += '<li>' + myCities[c] + '</li>';
+}
+cityHtmlCode += '</ul>';
+console.log(cityHtmlCode);
+el.insertAdjacentHTML('afterbegin', cityHtmlCode);
 
 el = document.getElementById('score');
 el.textContent = score;
