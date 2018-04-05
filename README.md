29d8d8ae338cd389c0973c7be90536865c25737b
Kevin Hwangpo <hwangpo3@gmail.com>
2018-04-05 14:10:58 -0700

    Pair assignment, functions.

--- README.md
+++ README.md
@@ -12,6 +12,8 @@ This program asks five questions about me (Tracy Williams). The user story is so
 
 ## Version History
 
+- April 5, 2018. pair assignment(Kevin, Tracy). We collaborated, assembled function statements. Kevin drove, Tracy navigated. 
+
 - April 4, 2018. Day 3 lab. added two more guessing games: Guess a number (number of my grand kids) and a Washington city I've lived in. Replaced reduandant code with simpler code inside for loops. Brought project up to Day 2 level (I'd missed some of the user story requirements).
 
 - April 3, 2018. Initial working version. Simple CSS. Code uses arrays and is ready for implementation of for loops.
--- js/app.js
+++ js/app.js
@@ -34,93 +34,108 @@ var userName; // user's name
 var numberOfGrandKids = 7; // the number to guess
 var guessCountDown = 4; // number of tries to guess it
 var userGuess; // user's guess at the number
+var myCities = ['Tacoma', 'Gig Harbor', 'Bellevue', 'Kirkland', 'Randle']; //array of cities.
+var cityGuess; //guessing the number of cities.
 
+
+//
+userName = getUsersName();
+playYesOrNoGame();
+guessMyNumber(numberOfGrandKids, guessCountDown);
+guessMyCity();
+updateTheHtml();
 //
 // getUsersName
 //
-userName = prompt('Welcome to my About Tracy game!\n\nWhat\'s your name?');
-if (!confirm('Thanks ' + userName + '! Ready to play my game?\n\n(I\'ll show you your results when we\'re all done playing.)')) {
-  alert('OK ' + userName + '. No hard feelings! (We\'ll play anyway...');
-  // this is where I'd like to abort the script...
+function getUsersName() {
+  var nameOfUser = prompt('Welcome to my About Tracy game!\n\nWhat\'s your name?');
+  if (!confirm('Thanks ' + nameOfUser + '! Ready to play my game?\n\n(I\'ll show you your results when we\'re all done playing.)')) {
+    alert('OK ' + nameOfUser + '. No hard feelings! (We\'ll play anyway...');
+  }
+  return nameOfUser;
 }
 
 //
-// askTheQuestions
+// playYesOrNoGame();
 //
 // This "function" loops through all the questions in the array 'questions' and gathers
 // the user's response.
 //
-for (var i = 0; i < questions.length; i++) {
-  guess[i] = prompt(questions[i]);
-  if ((guess[i].toUpperCase().charAt(0)) === answers[0][i]) {
-    score++;
-  }
-  console.log(questions[i], 'User input:', guess[i], 'Right answer:', answers[1][i],'\n');
-}
-alert('Thank you for playing! ' + userName + '. Ready to play a guessing game?');
-
-
-//Play the number guessing game
-
-console.log('grandKidCountdown',guessCountDown);
-do {
-  userGuess = parseInt(prompt('How many grandkids to I have?'));
-  guessCountDown--;
-  console.log('Grandkid guess:', userGuess, 'Guesses to go', guessCountDown);
-  console.log('typeof input',typeof userGuess);
-  if (!isNaN(userGuess)) { //then they entered a number...
-    if (userGuess === numberOfGrandKids) {
-      alert('That\'s right ' + userName + '! Nicely done.');
+function playYesOrNoGame(){
+  for (var i = 0; i < questions.length; i++) {
+    guess[i] = prompt(questions[i]);
+    if ((guess[i].toUpperCase().charAt(0)) === answers[0][i]) {
       score++;
-      break;
-    } else if (guessCountDown > 0) {
-      alert('Close ' + userName + ', but your a bit ' + (userGuess > numberOfGrandKids ? 'high.' : 'low. ' + guessCountDown + (guessCountDown === 1 ? ' guess' : ' guesses') + ' to go!'));
-    } else {
-      alert('Close ' + userName + ', but your a bit ' + (userGuess > numberOfGrandKids ? 'high.' : 'low. ') + 'And you\'re out of guesses!');
     }
-  } else {
-    alert('Please enter a number as your answer. ' + guessCountDown + (guessCountDown === 1 ? ' guess' : ' guesses') + ' to go!');
+    console.log(questions[i], 'User input:', guess[i], 'Right answer:', answers[1][i],'\n');
   }
-} while (guessCountDown > 0);
-console.log('done guessing');
+  alert('Thank you for playing! ' + userName + '. Ready to play a guessing game?');
+}
 
+//Play the number guessing game: guessMyNumber(numberOfGrandKids, guessCountDown);
+//This function uses a global variable called userName.
+function guessMyNumber(rightAnswer, numberOfTries){
+  var userGuess;
+  console.log(rightAnswer, numberOfTries);
+  do {
+    userGuess = parseInt(prompt('How many grandkids to I have?'));
+    numberOfTries--;
+    console.log('Grandkid guess:', userGuess, 'Guesses to go', numberOfTries);
+    console.log('typeof input',typeof userGuess);
+    if (!isNaN(userGuess)) { //then they entered a number...
+      if (userGuess === rightAnswer) {
+        alert('That\'s right ' + userName + '! Nicely done.');
+        score++;
+        break;
+      } else if (numberOfTries > 0) {
+        alert('Close ' + userName + ', but your a bit ' + (userGuess > rightAnswer ? 'high.' : 'low. ' + numberOfTries + (numberOfTries === 1 ? ' guess' : ' guesses') + ' to go!'));
+      } else {
+        alert('Close ' + userName + ', but your a bit ' + (userGuess > rightAnswer ? 'high.' : 'low. ') + 'And you\'re out of guesses!');
+      }
+    } else {
+      alert('Please enter a number as your answer. ' + numberOfTries + (numberOfTries === 1 ? ' guess' : ' guesses') + ' to go!');
+    }
+  } while (numberOfTries > 0);
+  console.log('done guessing');
+}
 //
 // guessMyCities...
 //
 // user has 6 tries to guess a city I've lived in...
-//
-var myCities = ['Tacoma', 'Gig Harbor', 'Bellevue', 'Kirkland', 'Randle'];
-var numTries = 6;
-var cityGuess;
-var correctGuess = false;
+// The userName is used as global variable.
 
-console.log('Guessing city...');
-alert('Okay ' + userName + '. Ready to play another guessing game?\n\nIn this one you have six tries to guess a Washington city I\'ve lived in.  Good luck!');
+function guessMyCity(){
 
-do {
-  cityGuess = prompt('Name a city I\'ve lived in:');
-  numTries--;
-  console.log('City guess', cityGuess, 'guesses remaining', numTries);
-  for (var c = 0; c < myCities.length; c++) {
-    if (cityGuess.toLowerCase() === myCities[c].toLowerCase()) {
-      alert('Great guess ' + userName + '! ' + myCities[c] + ' is a place I\'ve lived!');
-      correctGuess = true;
-      score++;
+  var numTries = 6;
+  var correctGuess = false;
+
+  console.log('Guessing city...');
+  alert('Okay ' + userName + '. Ready to play another guessing game?\n\nIn this one you have six tries to guess a Washington city I\'ve lived in.  Good luck!');
+
+  do {
+    cityGuess = prompt('Name a city I\'ve lived in:');
+    numTries--;
+    console.log('City guess', cityGuess, 'guesses remaining', numTries);
+    for (var c = 0; c < myCities.length; c++) {
+      if (cityGuess.toLowerCase() === myCities[c].toLowerCase()) {
+        alert('Great guess ' + userName + '! ' + myCities[c] + ' is a place I\'ve lived!');
+        correctGuess = true;
+        score++;
+        break;
+      }
+    }
+    if (correctGuess === false) {
+      var alertString = 'No, I haven\'t lived in ' + cityGuess + '. ';
+      if (numTries >= 1) {
+        alertString += 'Try again. You have ' + numTries + (numTries > 1 ? ' guesses' : ' guess') + ' remaining.';
+      }
+      alert(alertString);
+    } else {
       break;
     }
-  }
-  if (correctGuess === false) {
-    var alertString = 'No, I haven\'t lived in ' + cityGuess + '. ';
-    if (numTries >= 1) {
-      alertString += 'Try again. You have ' + numTries + (numTries > 1 ? ' guesses' : ' guess') + ' remaining.';
-    }
-    alert(alertString);
-  } else {
-    break;
-  }
-} while (numTries > 0);
-console.log('Finished cityGuess game, correctGuess flag:',correctGuess);
-
+  } while (numTries > 0);
+  console.log('Finished cityGuess game, correctGuess flag:',correctGuess);
+}
 //
 // updateTheHtml
 //
@@ -128,42 +143,44 @@ console.log('Finished cityGuess game, correctGuess flag:',correctGuess);
 // into the html.  Based on their score, some friendly trash talk is appended to the
 // results list.
 //
-var elIds = ['q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7'];
-var el = new Document;
+function updateTheHtml(){
+  var elIds = ['q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7'];
+  var el = new Document;
 
-el = document.getElementById('userName'); // Insert user's name
-el.textContent = userName;
+  el = document.getElementById('userName'); // Insert user's name
+  el.textContent = userName;
 
-// Fill in results of first five questions.
-for (var j = 0; j < questions.length; j++) {
-  el = document.getElementById(elIds[j]);
-  el.textContent = questions[j] + ' You answered ' + guess[j] + ', the correct answer was ' + answers[1][j] + '.';
-}
+  // Fill in results of first five questions.
+  for (var j = 0; j < questions.length; j++) {
+    el = document.getElementById(elIds[j]);
+    el.textContent = questions[j] + ' You answered ' + guess[j] + ', the correct answer was ' + answers[1][j] + '.';
+  }
 
-el = document.getElementById(elIds[5]); //element ID for question 6, number guess
-el.textContent = 'How many grand kids do I have? You guessed ' + (userGuess === numberOfGrandKids ? 'correctly! ' : 'incorrectly.') + ' The answer is ' + numberOfGrandKids + '.';
+  el = document.getElementById(elIds[5]); //element ID for question 6, number guess
+  el.textContent = 'How many grand kids do I have? You guessed ' + (userGuess === numberOfGrandKids ? 'correctly! ' : 'incorrectly.') + ' The answer is ' + numberOfGrandKids + '.';
 
-// Question 7. Did some googling here to find the insertAdjacentHTML method.
-// I build an unordered list inside element 7 of the ordered list of Q&A's.
-el = document.getElementById(elIds[6]);
-var cityHtmlCode = 'What cities have I lived in? You guessed ' + cityGuess + '. Here\'s a list of places I\'ve lived in Washinton:<ul>';
-for (c = 0; c < myCities.length; c++) {
-  cityHtmlCode += '<li>' + myCities[c] + '</li>';
-}
-cityHtmlCode += '</ul>';
-console.log(cityHtmlCode);
-el.insertAdjacentHTML('afterbegin', cityHtmlCode);
+  // Question 7. Did some googling here to find the insertAdjacentHTML method.
+  // I build an unordered list inside element 7 of the ordered list of Q&A's.
+  el = document.getElementById(elIds[6]);
+  var cityHtmlCode = 'What cities have I lived in? You guessed ' + cityGuess + '. Here\'s a list of places I\'ve lived in Washinton:<ul>';
+  for (var c = 0; c < myCities.length; c++) {
+    cityHtmlCode += '<li>' + myCities[c] + '</li>';
+  }
+  cityHtmlCode += '</ul>';
+  console.log(cityHtmlCode);
+  el.insertAdjacentHTML('afterbegin', cityHtmlCode);
 
-//Insert score into a <span> element in the HTML
-el = document.getElementById('score');
-el.textContent = score;
+  //Insert score into a <span> element in the HTML
+  el = document.getElementById('score');
+  el.textContent = score;
 
-//Add some trash talk relative to user's score...
-el = document.getElementById('comment-on-score');
-if (score === 7) {
-  el.textContent = 'A perfect score! You must know me pretty well. Nice job.';
-} else if (score >= 4 && score <= 6) {
-  el.textContent = 'Not bad. Better than mere random chance. You pass!';
-} else { // (score <= 3)
-  el.textContent = 'Man, that score blows. You need to have a conversation with me sometime soon and get some answers!';
+  //Add some trash talk relative to user's score...
+  el = document.getElementById('comment-on-score');
+  if (score === 7) {
+    el.textContent = 'A perfect score! You must know me pretty well. Nice job.';
+  } else if (score >= 4 && score <= 6) {
+    el.textContent = 'Not bad. Better than mere random chance. You pass!';
+  } else { // (score <= 3)
+    el.textContent = 'Man, that score blows. You need to have a conversation with me sometime soon and get some answers!';
+  }
 }
